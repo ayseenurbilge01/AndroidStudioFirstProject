2504f23e7e1fb521c2ed7d474ea680d799eb86cb
ayseenurbilge01 <47606155+ayseenurbilge01@users.noreply.github.com>
2021-04-18 17:01:39 +0300

    18.04.2021

--- app/src/main/java/com/example/ilkuygulama/profileFragment.java
+++ app/src/main/java/com/example/ilkuygulama/profileFragment.java
@@ -1,18 +1,56 @@
 package com.example.ilkuygulama;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import static android.content.ContentValues.TAG;
+
 public class profileFragment extends Fragment {
+Button HesabimiSil;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile,container,false);
+        View view = inflater.inflate(R.layout.fragment_profile,container,false);
+        HesabimiSil=(Button) view.findViewById(R.id.hesapSil);
+        HesabimiSil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseUser user =FirebaseAuth.getInstance().getCurrentUser();
+                user.delete().addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(getActivity(),"Hesabýnýz baþarýyla silindi.", Toast.LENGTH_SHORT).show();
+
+                            Intent intent =new Intent(getActivity(),karsilamaEkrani.class);
+                           startActivity(intent);
+                        }
+                    }
+                });
+
+            }
+
+        });
+        return view;
+
     }
+
+
 }
